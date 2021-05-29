70a9e97218461ae448551ee6f1fa16383f8b2a37
ezequiel <bugnonezequiel@gmail.com>
2021-05-29 20:33:46 -0300

    add config and start server

---
+++ backend/src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+class Server {
+    private app: express.Application;
+
+    constructor() {
+        this.app = express();
+    }
+
+    private config():void{
+        this.app.set('port', process.env.PORT || 4000);
+
+        this.app.use(morgan('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cors());
+    }
+
+    public start():void {
+        this.config();
+        this.app.listen(this.app.get('port'), 
+        () => console.log('Server is listenning on port', this.app.get('port')))
+    }
+
+}
+
+const server = new Server();
+
+server.start();
